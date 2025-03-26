ca5a0f38ecc6bf7df84859c5ef92fde9dcff3cdc
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-03-26 15:03:33 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,46 @@
+// import logo from './logo.svg';
+import { useState } from 'react'
+import './App.css';
 
+//component to avoid repetition
+function Square({ value }) {
+  const [name, setName] = useState(null);
+
+  function handleButtonClick() {
+    setName("X")
+    console.log("Click action", name);
+  }
+
+  return (
+    <button onClick={handleButtonClick} className="square">{name}</button>
+  );
+
+}
+function App() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+
+      </div>
+
+    </>
+
+  );
+}
+
+export default App;
